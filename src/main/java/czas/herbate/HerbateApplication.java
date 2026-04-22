1f278fc50632e2e638d9265a1bc6ad75190adfaa
edwardlee <openmoresome@gmail.com>
2026-04-22 21:24:52 +0900

    opened new repository

---
+++ src/main/java/czas/herbate/HerbateApplication.java
@@ -0,0 +1,13 @@
+package czas.herbate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerbateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/czas/herbate/HerbateApplicationTests.java
@@ -0,0 +1,13 @@
+package czas.herbate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
