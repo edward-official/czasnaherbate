6dd5715ade2db546dd634d1c031764e377d8a88b
edwardlee <openmoresome@gmail.com>
2026-04-24 23:44:15 +0900

    section 3 (basic): lecture

--- src/main/java/czas/herbate/controller/CzasController.java
+++ src/main/java/czas/herbate/controller/CzasController.java
@@ -3,12 +3,42 @@ package czas.herbate.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class CzasController {
     @GetMapping("czas")
     public String czas(Model model) {
+        // Model: data interface designed to transport processed data from the Controller to the View (template engine)
         model.addAttribute("name", "edward lee");
         return "czas"; // viewResolver: viewName mapping (find czas.html)
+    }
+
+    @GetMapping("response-body")
+    @ResponseBody()
+    public Player responseBody() {
+        Player player = new Player();
+        return player;
+    }
+
+    static class Player {
+        private int backNumber;
+        private String name;
+
+        public int getBackNumber() {
+            return backNumber;
+        }
+
+        public void setBackNumber(int backNumber) {
+            this.backNumber = backNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
