3c4e8a6ed32fa4e717ecbf362108516f23adba9e
edwardlee <openmoresome@gmail.com>
2026-07-25 16:07:47 +0900

    section 6 (mvc): comeback

--- src/main/java/czas/herbate/controller/MemberController.java
+++ src/main/java/czas/herbate/controller/MemberController.java
@@ -1,8 +1,14 @@
 package czas.herbate.controller;
 
+import czas.herbate.domain.Member;
 import czas.herbate.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /**
  * @Controller: A Spring Bean annotation for the presentation layer.
@@ -16,12 +22,35 @@ public class MemberController {
 
     /**
      * @Autowired: Explicitly tells Spring to inject the MemberService dependency.
-     * For constructor injection (like here), if there's only one constructor, 
+     * For constructor injection (like here), if there's only one constructor,
      * @Autowired can often be omitted, but it's good practice for clarity.
      */
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.signUp(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 
 }
