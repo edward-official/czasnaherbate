d9c57061380a3b5e273c0666919f0e33072db683
edwardlee <openmoresome@gmail.com>
2026-09-20 17:25:23 +0900

    section 7 (db): Spring Data JPA

--- src/main/java/czas/herbate/SpringConfig.java
+++ src/main/java/czas/herbate/SpringConfig.java
@@ -1,37 +1,22 @@
 package czas.herbate;
 
-import czas.herbate.repository.JdbcMemberRepository;
-import czas.herbate.repository.JdbcTemplateMemberRepository;
-import czas.herbate.repository.JpaMemberRepository;
 import czas.herbate.repository.MemberRepository;
 import czas.herbate.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    // Spring Data JPA auto-registers SpringDataJpaMemberRepository as the MemberRepository bean.
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
